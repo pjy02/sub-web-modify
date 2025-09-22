e2d10daaea75487802f69659e9da7afded5c278a
pjy02 <61741153+pjy02@users.noreply.github.com>
2025-09-22 15:28:25 +0800

    feat(Subconverter): 添加自建后端短链接选项
    在订阅转换器中新增"自建后端"作为短链接生成选项。

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -332,6 +332,7 @@ export default {
           "自动判断客户端": "auto",
         },
         shortTypes: {
+          "自建后端": "https://subapi.239888.xyz/short",
           "v1.mk": "https://v1.mk/short",
           "d1.mk": "https://d1.mk/short",
           "dlj.tf": "https://dlj.tf/short",
